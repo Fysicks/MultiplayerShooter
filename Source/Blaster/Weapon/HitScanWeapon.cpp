e676df413be407ff7d576b020351976f437cf6d5
Alex Grant <alexgrant9699@gmail.com>
2026-04-20 13:14:50 -0400

    Added smoke trails to hitscan weapons

--- Source/Blaster/Weapon/HitScanWeapon.cpp
+++ Source/Blaster/Weapon/HitScanWeapon.cpp
@@ -4,18 +4,21 @@
 #include "Engine/SkeletalMeshSocket.h"
 #include "Blaster/Character/BlasterCharacter.h"
 #include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystemComponent.h"
 
 void AHitScanWeapon::Fire(const FVector& HitTarget) {
 	Super::Fire(HitTarget);
 
 	APawn* OwnerPawn = Cast<APawn>(GetOwner());
 	if (OwnerPawn == nullptr) return;
-
 	AController* InstigatorController = OwnerPawn->GetController();
+	if (!HasAuthority() && InstigatorController) {
+		UE_LOG(LogTemp, Warning, TEXT("Instigator invalid"))
+	}
 
 	// Perform line trace
 	const USkeletalMeshSocket* MuzzleFlashSocket = GetWeaponMesh()->GetSocketByName("MuzzleFlash");
-	if (MuzzleFlashSocket && InstigatorController) {
+	if (MuzzleFlashSocket) {
 		FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetWeaponMesh());
 		FVector Start = SocketTransform.GetLocation();
 		FVector End = Start + (HitTarget - Start) * 1.25; // End location just past hit target to ensure a hit target
@@ -29,18 +32,19 @@ void AHitScanWeapon::Fire(const FVector& HitTarget) {
 				End,
 				ECollisionChannel::ECC_Visibility
 			);
+
+			FVector BeamEnd = End; // Will get overwritten to blocking hit if there is one
 			if (FireHit.bBlockingHit) {
+				BeamEnd = FireHit.ImpactPoint;
 				ABlasterCharacter* BlasterCharacter = Cast<ABlasterCharacter>(FireHit.GetActor());
-				if (BlasterCharacter) {
-					if (HasAuthority()) {
-						UGameplayStatics::ApplyDamage(
-							BlasterCharacter,
-							Damage,
-							InstigatorController,
-							this,
-							UDamageType::StaticClass()
-						);
-					}
+				if (BlasterCharacter && HasAuthority() && InstigatorController) {
+					UGameplayStatics::ApplyDamage(
+						BlasterCharacter,
+						Damage,
+						InstigatorController,
+						this,
+						UDamageType::StaticClass()
+					);
 				}
 				if (ImpactParticles) {
 					UGameplayStatics::SpawnEmitterAtLocation(
@@ -49,6 +53,16 @@ void AHitScanWeapon::Fire(const FVector& HitTarget) {
 						FireHit.ImpactPoint,
 						FireHit.ImpactNormal.Rotation()
 					);
+				}
+			}
+			if (BeamParticles) {
+				UParticleSystemComponent* Beam = UGameplayStatics::SpawnEmitterAtLocation(
+					World,
+					BeamParticles,
+					SocketTransform
+				);
+				if (Beam) {
+					Beam->SetVectorParameter(FName("Target"), BeamEnd);
 				}
 			}
 		}
