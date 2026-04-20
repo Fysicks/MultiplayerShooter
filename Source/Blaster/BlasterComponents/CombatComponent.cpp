60402e6645afdd92d25c6178278e880d7b920cba
Alex Grant <alexgrant9699@gmail.com>
2026-04-20 13:01:10 -0400

    Implemented hitscan weapons, first of which being a pistol

--- Source/Blaster/BlasterComponents/CombatComponent.cpp
+++ Source/Blaster/BlasterComponents/CombatComponent.cpp
@@ -397,4 +397,5 @@ void UCombatComponent::OnRep_CarriedAmmo() {
 void UCombatComponent::InitializeCarriedAmmo() {
 	CarriedAmmoMap.Emplace(EWeaponType::EWT_AssaultRifle, StartingARAmmo);
 	CarriedAmmoMap.Emplace(EWeaponType::EWT_RocketLauncher, StartingRocketAmmo);
+	CarriedAmmoMap.Emplace(EWeaponType::EWT_Pistol, StartingPistolAmmo);
 }
--- Source/Blaster/Weapon/WeaponTypes.h
+++ Source/Blaster/Weapon/WeaponTypes.h
@@ -4,5 +4,6 @@ UENUM(BlueprintType)
 enum class EWeaponType : uint8 {
 	EWT_AssaultRifle UMETA(DisplayName = "Assualt Rifle"),
 	EWT_RocketLauncher UMETA(DisplayName = "Rocket Launcher"),
+	EWT_Pistol UMETA(DisplayName = "Pistol"),
 	EWT_MAX UMETA(DisplayName = "Default Max")
 };
